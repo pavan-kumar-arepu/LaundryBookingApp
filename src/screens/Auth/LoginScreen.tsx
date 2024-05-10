0afbc2f367b373af3afd999771f972b71695be5d
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-05-10 17:21:57 +0200

    Added files related to AuthModule. Errors are there, yet to fix

--- src/screens/Auth/LoginScreen.tsx
+++ src/screens/Auth/LoginScreen.tsx
@@ -1,1 +1,38 @@
 // Login screen component
+
+// src/screens/Auth/LoginScreen.tsx
+
+import React, { useState } from 'react';
+import { View, TextInput, Button } from 'react-native';
+import { useDispatch } from 'react-redux';
+import { login } from '../../services/authServices';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+
+  const handleLogin = () => {
+    //dispatch(login(email, password));
+    // dispatch(login(email, password));
+  };
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+};
+
+export default LoginScreen;
---
+++ src/services/authServices.ts
@@ -0,0 +1,49 @@
+// Authentiation Services Module
+
+// src/services/authService.ts
+
+import auth from '@react-native-firebase/auth';
+import { Dispatch } from 'redux';
+import React, { useState } from 'react';
+import { User } from '../types';
+
+import { loginSuccess, loginFailure, logoutSuccess } from '../store/actions/authActions';
+
+// export const login = (email: string, password: string) => async (dispatch: Dispatch) => {
+//   try {
+//     const userCredential = await auth().signInWithEmailAndPassword(email, password);
+//     console.log("userCredentails received");
+//     // dispatch(loginSuccess(userCredential.user));
+//   } catch (error) {
+//     // dispatch(loginFailure(error.message));
+//     console.log("Error Received")
+//   }
+// };
+
+
+export const login = (email: string, password: string) => async (dispatch: (arg0: { payload: User; type: string; }) => void) => {
+  try {
+    const userCredentails = await auth().signInWithEmailAndPassword(email,password);
+    console.log(userCredentails.user);    
+    
+// Create a user object using data from userCredentials
+const user: User = {
+  name: userCredentails.user.displayName || '',
+  email: userCredentails.user.email || ''
+};
+
+dispatch(loginSuccess(user)); // Dispatch login success action with user object as payload
+  } catch (error) {
+    console.error('Error logging out:', error);
+  }
+};
+
+
+export const logout = () => async (dispatch: Dispatch) => {
+  try {
+    await auth().signOut();
+    dispatch(logoutSuccess());
+  } catch (error) {
+    console.error('Error logging out:', error);
+  }
+};
